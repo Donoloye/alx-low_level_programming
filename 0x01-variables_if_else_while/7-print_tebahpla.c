d58cd29c80834ea2e4f7ec00b8616d5a4fce6c8f
Donoloye <damilare2010@yahoo.com>
2022-04-08 17:45:21 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
 /**
-  * main 
-  *
-  * Return: Always (Success)
-  */
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char c;
+	char ch;
 
-	for (c = 'z'; c >= 'a'; c--)
+	for (ch = 'z'; ch >= 'a'; ch--)
 	{
-		putchar(c);
+		putchar(ch);
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 
 /**
-  * main
-  *
-  * Return: Always (Success)
-  */
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char c;
+	int n;
+	char ch;
 
-	for (c = '0'; c <= '9'; c++)
+	for (n = 48; n < 58; n++)
 	{
-		putchar(c);
+		putchar(n);
 	}
-
-	for (c = 'a'; c <= 'f'; c++)
+	for (ch = 'a'; ch <= 'f'; ch++)
 	{
-		putchar(c);
+		putchar(ch);
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
