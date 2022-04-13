42079307907ad9b20a480b168e4adc39ee622485
Donoloye <damilare2010@yahoo.com>
2022-04-12 17:15:12 -0700

    printing alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char ab = 'a';
+
+	while (ab <= 'z')
+	{
+		_putchar(ab);
+		ab++;
+
+	}
+	_putchar('\n');
+
+}
