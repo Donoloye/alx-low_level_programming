cf859aaa3479ade7f407900eb620e27b72daa321
Donoloye <damilare2010@yahoo.com>
2022-07-07 19:28:14 -0700

    learning doubly linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,45 +1,46 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Insert a new node in the given index
- * @h: Memory address of the head list
- * @idx: Index of the list
- * @n: New data (int)
- *
- * Return: the address of the new node, or NULL if it failed
+ * insert_dnodeint_at_index - insert node at specific index
+ * @h: head of linked list
+ * @idx: index of new node
+ * @n: new node value
+ * Return: inserted node
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new = NULL;
-	dlistint_t *head = *h, *prev = *h;
-	unsigned int i;
+	dlistint_t *current;
+	dlistint_t *new;
 
-	if (h == NULL || (head == NULL && idx > 0))
-		return (NULL);
-	if (idx == 0)
-		return (add_dnodeint(h, n));
+	if (h == NULL)
+		return (0);
 
-	head = head->next;
-	for (i = 1; i < idx; i++)
+	current = *h;
+
+	while (idx != 0)
 	{
-		if (prev->next == NULL)
+		current = current->next;
+		idx--;
+		if (current == NULL)
 			return (NULL);
-		head = head->next;
-		prev = prev->next;
 	}
-	if (head == NULL)
-		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
+
 	if (new == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
+
 	new->n = n;
-	new->next = head;
-	new->prev = prev;
-	prev->next = new;
-	if (head != NULL)
-		head->prev = new;
-	return (new);
+	new->next = current;
+	new->prev = current->prev;
+	if (current->prev != NULL)
+		current->prev->next = new;
+
+	/*TODO: Handle special case when idx is 0 and last index*/
+
+	return (current);
 }
-Footer
-© 2022 GitHu
